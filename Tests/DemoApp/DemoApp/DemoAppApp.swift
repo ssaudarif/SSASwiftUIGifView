e15afe15cdf4c068afd1cf05f9c12e976749964b
Syed Saud Arif <>
2022-01-31 21:30:27 +0530

    Initial DemoApp Commit.

---
+++ Tests/DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Syed Saud Arif on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
